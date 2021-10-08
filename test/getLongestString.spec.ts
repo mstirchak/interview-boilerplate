361fb1feb13126901294bb4c1b399a8e2813cd18
Michael Stirchak <michael.stirchak@limelighthealth.com>
2021-10-07 17:20:10 -0700

    base case added

--- test/getLongestString.spec.ts
+++ test/getLongestString.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it } from 'mocha';
-import {expect} from 'chai';
+import { expect } from 'chai';
 import { getLongestString } from '../src/getLongestString'
 
-describe('getLongestString', function(){
-  it("should return empty if input is empty", function(){
-    const inputString: string = "";
+describe('getLongestString', function () {
+  it('should return the longest string in a sentence', function () {
+    const inputString: string = 'What is the longest word in this string';
     const longestString: string = getLongestString(inputString);
-    expect(longestString).to.eq('');
+    expect(longestString).to.eq('longest');
   });
 });
